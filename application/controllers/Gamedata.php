3fb23deb46685d3b7ed3859c1310df6c5e651d98
codylin <chclin@gmail.com>
2016-04-14 14:39:36 -0700

    get recent transactions Signed-off-by:codylin <chclin@gmail.com>

--- application/controllers/Gamedata.php
+++ application/controllers/Gamedata.php
@@ -23,9 +23,13 @@ class Gamedata extends Application {
         $this->data['title'] = 'GameData';
         $this->data['pagebody'] = 'gamedata';
         //$this->data['info'] = print_r($this->test->get_all_bot_names());
-        print_r($this->test->get_bot_by_code('11'));
-        echo $this->test->get_bot_value('11');
+        //print_r($this->test->get_bot_by_code('11'));
+        //echo $this->test->get_bot_value('11');
+        //print_r($this->test->get_certificates_data());
+        //print_r($this->test->get_transactions_data());
+        print_r($this->test->get_player_recent_trans('billy', 3));
         //print_r($this->test->get_series_data());
+        //echo $this->state->get_round();
         //echo $this->state->get_round();
 
               $this->render();
