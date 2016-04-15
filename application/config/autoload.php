4693c610caa4c03bf38a3924190e4052fcb80aa2
Rin <Rin@Rin-HP>
2016-04-14 17:01:31 -0700

    Merge upstream/registration into statusDisplay
    Conflicts:
    	application/models/State.php
    Signed-off-by:Rin <Rin@Rin-HP>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -137,4 +137,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('piece','player', 'transaction','access', 'test', 'state', 'register');
+$autoload['model'] = array('piece','player', 'transaction','access', 'data', 'state', 'register');
